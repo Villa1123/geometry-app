1a6b4744c86b5d9d47103e5f6c94f27042f6ae2f
ChefenML <milo1000@stud.ek.dk>
2026-04-24 13:51:13 +0200

    Rectangle

--- src/main/java/Shapes/Rectangle.java
+++ src/main/java/Shapes/Rectangle.java
@@ -1,4 +1,37 @@
 package Shapes;
 
 public class Rectangle {
+
+    private double width,height;
+
+    public Rectangle(double width, double height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double calculateRectangleArea(double width, double height) {
+        return width * height;
+    }
+
+    public double calculateRectanglePerimeter(double width, double height) {
+        return 2 * (width + height);
+    }
+
+
 }
