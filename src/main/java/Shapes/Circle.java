2b0e363ac2a5860e280d76a54aaae3a4dbf2c0e1
AmandaHult98 <amhu1000@stud.ek.dk>
2026-04-24 13:57:36 +0200

    Circle class

--- src/main/java/Shapes/Circle.java
+++ src/main/java/Shapes/Circle.java
@@ -2,6 +2,14 @@ package Shapes;
 
 public class Circle implements Shape {
 
+    private double width;
+    private double height;
+
+    public Circle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
     public double area(double radius) {
         return Math.PI * radius * radius;
     }
