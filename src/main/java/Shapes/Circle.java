58f26728230424739d7579ca2963dd8a423cafec
AmandaHult98 <amhu1000@stud.ek.dk>
2026-04-24 14:34:35 +0200

    Circle class ændringer

--- src/main/java/Shapes/Circle.java
+++ src/main/java/Shapes/Circle.java
@@ -2,20 +2,18 @@ package Shapes;
 
 public class Circle implements Shape {
 
-    private double width;
-    private double height;
+    private double radius;
 
-    public Circle(double width, double height) {
-        this.width = width;
-        this.height = height;
+    public Circle(double radius) {
+        this.radius = radius;
     }
 
-    public double area(double radius) {
+    public double area() {
         return Math.PI * radius * radius;
     }
 
 
-    public double perimeter(double radius) {
+    public double perimeter() {
         return 2 * Math.PI * radius;
     }
 
